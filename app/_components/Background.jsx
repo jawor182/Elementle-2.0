35ecfae8842663a452f33eb787207c9b6dbce27a
jawor182 <kubajaworski59@outlook.com>
2025-01-30 21:33:14 +0100

    bg

--- app/_components/Background.jsx
+++ app/_components/Background.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import bg from "../../public/bg.png"
+import bg from "../../public/bg.png";
 
 const Background = () => {
   return (
     <div className="relative w-full flex-1 bg-zinc-500 -z-10">
-        <Image
+      <Image
         alt="Tło"
         src={bg}
         quality={100}
+        priority
         fill
-        style={
-            {
-                objectFit: "cover",
-                zIndex:-1,
-            }
-        }
-        >
-        </Image>
+        className="h-screen"
+        style={{
+          objectFit: "cover",
+        }}
+      />
     </div>
   );
 };
