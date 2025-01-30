7a992398c89a6c50faf60036d4ef4394b2db643c
jawor182 <kubajaworski59@outlook.com>
2025-01-30 17:55:18 +0100

    header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
     return (
-        <div className="bg-slate-500 w-full no-underline h-6">
-            <div className="hover:underline no-underline" href="/ranking">Ranking</div>
-            <div className="hover:underline " href="/o-nas">O Nas</div>
-            <div className="hover:underline " href="/ustawienia">Ustawienia</div>
-            <div className="hover:underline " href="/profil">Profil</div>
+        <div className="bg-zinc-800 w-full no-underline h-12 flex justify-around items-center text-white ">
+            <Link className="hover:underline hover:text-orange-100" href="/ranking">Ranking</Link>
+            <Link className="hover:underline hover:text-orange-100" href="/o-nas">O Nas</Link>
+            <Link className="hover:underline hover:text-orange-100" href="/ustawienia">Ustawienia</Link>
+            <Link className="hover:underline hover:text-orange-100" href="/profil">Profil</Link>
         </div>
     )
 };
--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,19 @@
+import "./globals.css"
+import { Roboto } from "next/font/google"
+
 export const metadata = {
   title: "Elementle",
   description: "Elementle",
 };
 
+const roboto = Roboto({subsets:['latin'], weight:"400"});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.className}>
+        {children}
+        </body>
     </html>
   );
 }
