822726f1e70209dd357632639e5ec1863501611b
jawor182 <kubajaworski59@outlook.com>
2025-03-26 22:14:01 +0100

    layout

--- app/games/layout.js
+++ app/games/layout.js
@@ -4,9 +4,11 @@ import Footer from "../_components/Footer";
 export default function GameLayout({ children }) {
   return (
     <div className="relative flex flex-col min-h-screen">
-      <Header/>
-      {children}
-      <Footer/>
+      <Header />
+      <div className="relative flex-1 bg-bgImg bg-repeat bg-cover bg-zinc-500">
+        {children}
+      </div>
+      <Footer />
     </div>
   );
 }
--- app/games/wlasciwosci/page.js
+++ app/games/wlasciwosci/page.js
@@ -3,24 +3,22 @@ import { fetchElementle } from "@/lib/firebase";
 import React, { useCallback, useEffect, useRef, useState } from "react";
 
 function page() {
-    const [elementle, setElementle] = useState(null);
-    const hasFetched = useRef(false);
+  const [elementle, setElementle] = useState(null);
+  const hasFetched = useRef(false);
 
-    useEffect(() => {
-        if (!hasFetched.current) {
-            hasFetched.current = true;
-            async function fetchData() {
-                const response = await fetchElementle();
-                setElementle(response.pierwiastki);
-            }
-            fetchData();
-        }
-    }, []);
-    console.log('Elementle:', elementle || 'Loading...');
-    // TODO: karta - buttony do porownanie pierwiastkow i stan jej
-    return (
-        <div className="relative flex-1 bg-bgImg bg-repeat bg-cover bg-zinc-500"></div>
-    );
+  useEffect(() => {
+    if (!hasFetched.current) {
+      hasFetched.current = true;
+      async function fetchData() {
+        const response = await fetchElementle();
+        setElementle(response.pierwiastki);
+      }
+      fetchData();
+    }
+  }, [hasFetched, setElementle, fetchElementle]);
+  console.log("Elementle:", elementle || "Loading...");
+  // TODO: karta - buttony do porownanie pierwiastkow i stan jej
+  return <div>Test</div>;
 }
 
 export default page;
